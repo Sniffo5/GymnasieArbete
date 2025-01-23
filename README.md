65ec9d04527d8414c5bac2626f2a2c296699b628
Aron Loreskär <arolor0730@edu.halmstad.se>
2025-01-23 09:24:40 +0100

    start

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# GymnasieArbete
+# Gymnasiearbete
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const express = require("express");
+const express = require('express');
+const bcrypt = require("bcryptjs");
+const session = require('express-session');
+const app = express();
+app.use(express.urlencoded({extended:true}));
+const fs = require("fs");
+
+app.use(express.static("public"));
+app.listen(3000, () => console.log("http://localhost:3000/"));
+
+app.get("/", home);
+
+function home(req,res){
+
+    html = fs.readFileSync("./template.html").toString();
+
+    res.send(html);
+}
